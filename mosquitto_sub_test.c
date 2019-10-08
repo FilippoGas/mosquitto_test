b13f162dfa847be6b60488ea2d35fb934c13569f
Filippo <filippogastaldello@gmail.com>
2019-10-08 19:33:17 +0200

    Subscriber
    first implementation of mosquitto_sub, can only subscribe to the topic specified in the code.
    
    To do:
    -Remove useless functions
    -Add parameter to change host/topic

---
+++ mosquitto_sub_test.c
@@ -0,0 +1,93 @@
+#include<mosquitto.h>
+#include<stdio.h>
+
+
+
+
+void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str)
+{
+    printf("%s\n",str);
+};
+
+void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
+{
+    if (!result)
+    {
+            mosquitto_subscribe(mosq, NULL, "$SYS/#",2); //test non sys
+    }else
+    {
+        fprintf(stderr, "Connection failed.\n");
+    }
+    
+    
+};
+
+
+void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
+{
+    if (message->payloadlen)    
+    {
+        printf("%s %s\n", message->topic, message->payload);
+    }else{
+        printf("%s (null)\n",message->topic);
+    }
+    fflush(stdout);
+    
+};
+
+
+void my_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos){
+
+    int i;
+    printf("Subscribed (mid : %d):%d", mid, granted_qos[0]);
+    for (i = 1; i < qos_count; i++)
+    {
+        printf(", %d",granted_qos[i]);
+    }
+    printf("\n");
+
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int port = 1883;
+    char *host = "localhost";
+    int keepalive = 60;
+    bool clean_session = true;
+    struct mosquitto *mosq = NULL;
+
+
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new(NULL,clean_session,NULL);
+    if (!mosq)
+    {
+        fprintf(stderr,"ERROR: out of memory.\n");
+        return 1;
+    }
+    
+    mosquitto_log_callback_set(mosq,my_log_callback);
+    mosquitto_connect_callback_set(mosq, my_connect_callback);
+    mosquitto_message_callback_set(mosq, my_message_callback);
+    mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
+
+
+
+    mosquitto_publish();
+    if (mosquitto_connect(mosq,host, port, keepalive))
+    {
+        fprintf(stderr, "Unable to connect.\n");
+        return 1;
+    }
+    
+    mosquitto_loop_forever(mosq,-1,1);
+
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
