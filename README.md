e40a6b86951b78ba8b1fac3e9c34f404d627f4cb
Filippo <filippogastaldello@gmail.com>
2019-10-09 12:17:26 +0200

    parameters
    Localhost and host can now be defined from command line

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Basic implementation of MQTT protocoll with mosquitto
 
 
 -----SUBSCRIBER-------
-Compile using:  gcc -o mosquitto_sub_test mosquitto_sub_test.c -libmosquitto
+Compile using:  gcc -o [name] mosquitto_sub_test.c -libmosquitto
+execue using: ./[name] [localhost] [topic]
 
 host: localhost
 topic: test
--- mosquitto_sub_test.c
+++ mosquitto_sub_test.c
@@ -1,33 +1,18 @@
 #include<mosquitto.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 
 
 
-void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str)
-{
-    printf("%s\n",str);
-};
-
-void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
-{
-    if (!result)
-    {
-            mosquitto_subscribe(mosq, NULL, "test",2);
-    }else
-    {
-        fprintf(stderr, "Connection failed.\n");
-    }
-    
-    
-};
 
 
 void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
 {
     if (message->payloadlen)    
     {
-        printf("%s %s\n", message->topic, message->payload);
+        printf("topic:%s, message:%s\n", message->topic, message->payload);
     }else{
         printf("%s (null)\n",message->topic);
     }
@@ -36,18 +21,6 @@ void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mo
 };
 
 
-void my_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos){
-
-    int i;
-    printf("Subscribed (mid : %d):%d", mid, granted_qos[0]);
-    for (i = 1; i < qos_count; i++)
-    {
-        printf(", %d",granted_qos[i]);
-    }
-    printf("\n");
-
-}
-
 
 
 
@@ -55,12 +28,20 @@ int main(int argc, char const *argv[])
 {
     int i;
     int port = 1883;
-    char *host = "localhost";
     int keepalive = 60;
     bool clean_session = true;
     struct mosquitto *mosq = NULL;
 
+    char *host = (char *)malloc(0);
+    char *topic = (char *)malloc(0);
+    
+    host = (char *)realloc(host, (strlen(argv[1])));
+    strcat(host, argv[1]);
+    
+    topic = (char *)realloc(topic, (strlen(argv[2])));
+    strcat(topic, argv[2]);
 
+    
     mosquitto_lib_init();
 
     mosq = mosquitto_new(NULL,clean_session,NULL);
@@ -70,19 +51,20 @@ int main(int argc, char const *argv[])
         return 1;
     }
     
-    mosquitto_log_callback_set(mosq,my_log_callback);
-    mosquitto_connect_callback_set(mosq, my_connect_callback);
     mosquitto_message_callback_set(mosq, my_message_callback);
-    mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
 
 
-
-    mosquitto_publish();
     if (mosquitto_connect(mosq,host, port, keepalive))
     {
         fprintf(stderr, "Unable to connect.\n");
         return 1;
     }
+
+    if (mosquitto_subscribe(mosq, NULL, topic, 2))
+    {
+        fprintf(stderr,"Connection failed.\n");
+    }
+    
     
     mosquitto_loop_forever(mosq,-1,1);
 
